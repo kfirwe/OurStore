05fd8bc49b63bdb80b64de3224a7111282960e90
kfirwe <kfir.weissman02@gmail.com>
2024-09-08 20:25:13 +0300

    added hashed password

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,7 +8,7 @@ router.post("/login", async (req, res) => {
 
   try {
     // Find the user by username and password
-    const user = await User.findOne({ username, password });
+    const user = await User.findOne({ username });
 
     if (!user) {
       await createLog("INFO", username, "Failed login attempt.");
@@ -19,6 +19,13 @@ router.post("/login", async (req, res) => {
 
     // Check if the user is an admin
     const isAdmin = user.role === "admin";
+
+    // Compare input password with the hashed password in the database
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      await createLog("INFO", username, "Failed login attempt.");
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
 
     req.session.user = {
       id: user._id,
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { purchaseSchema } = require("./Purchase"); // Correctly import purchaseSchema
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -33,6 +34,27 @@ const userSchema = new mongoose.Schema({
     default: "customer",
   },
 });
+
+// Hash the password before saving the user model
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next(); // Only hash the password if it has been modified (or is new)
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10); // Generate salt with cost factor 10
+    this.password = await bcrypt.hash(this.password, salt); // Hash the password with the salt
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare input password with hashed password in the database
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  console.log(candidatePassword, this.password);
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema, "users");
 
