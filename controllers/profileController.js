869664c23cc9bf2f0a9e5c18c4b61cedde738300
AmitAvisar14 <amitavisar6@gmail.com>
2024-09-19 17:39:35 +0300

    added order history

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -98,5 +98,58 @@ router.post(
   }
 );
 
+// Example route using the middleware
+router.get("/profile/purchase-history", ensureAuthenticated, async (req, res) => {
+  try {
+    // Fetch the current user's username
+    const username = req.session.user ? req.session.user.username : "";
+    if (!username){
+      await createLog("INFO", username, "User feth attempt failed. User not found");
+      return res
+          .status(404)
+          .json({message: "User data fetch failed. Invalid username or password."})
+    }
+
+    // Fetch user with username
+    const user = await User.findOne({ username })
+    if (!user){
+      await createLog("INFO", username, "User fetch attempt Failed. User not found");
+      return res
+        .status(404)
+        .json({ message: "User data fetch failed. Invalid username or password." });
+    }
+
+    // Set number of orders made by user
+    const userPurchases = user.purchaseHistory;
+    let purchasesCount = 0;
+
+    // If user has made an order set number of orders and get latest order details
+    if (userPurchases && userPurchases.length > 0){
+      purchasesCount = userPurchases.length;
+    }
+
+    // Fetch the cart for the current user
+    let cartItemCount = 0;
+    if (username) {
+      const cart = await Cart.findOne({ userName: username });
+      if (cart) {
+        cartItemCount = cart.products.length; // Count the number of distinct items in the cart
+      }
+    }
+
+    res.render("orderHistory", {
+      isAdmin: req.session.user ? req.session.user.role === "admin" : false, // Check if the user is an admin - for NavBar
+      username,
+      user,
+      userPurchases,
+      purchasesCount,
+      cartItemCount // Number of distinct items in the cart  for NavBar
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
 
 module.exports = router;
